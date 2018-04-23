082dff0a55aa7984fda6fad216ba287d7b6feb93
BrayanLoachamin <38636002+BrayanLoachamin@users.noreply.github.com>
2018-04-22 19:44:12 -0500

    Add files via upload

---
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,24 @@
+package ejercicio1;
+
+        
+/**
+ *
+ * @author Brayan Loachamin
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     double fahrenheit = 43;
+     double celsius;
+          
+     System.out.println("Grados fahrenheit = " + fahrenheit);
+     System.out.println("La conversion de fahrenheit a celsius es: ");
+     System.out.println(((fahrenheit-32)*5)/9);
+    }
+    
+}
+
