4adf44555fc82283a894facb133ad91cb408f477
BrayanLoachamin <38636002+BrayanLoachamin@users.noreply.github.com>
2018-04-22 19:50:11 -0500

    Add files via upload

---
+++ Ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -0,0 +1,47 @@
+package ejercicio6;
+import java.util.Scanner;
+/**
+ *
+ * @author Brayan Loachamin
+ */
+public class Ejercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        int aux=0; //Esta variable sera usada para ingresar los valores  positivo y negativo
+        boolean nevando=false;
+        boolean lloviendo=true;
+        double temperatura=60.0;
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("INFORME DEL CLIMA");
+        System.out.println("Si esta nevando ingrese '1' caso contrario ingrese '0'");
+        aux=scan.nextInt();
+        if (aux==1) {
+            nevando = true;
+        }else{
+            nevando= false;
+        }
+        System.out.println("Si esta lloviendo ingrese '1' caso contrario ingrese '0'");
+        aux=scan.nextInt();
+        if (aux==1) {
+            lloviendo = true;
+        }else{
+            lloviendo = false;
+        }
+        System.out.println("Ingrese la temperatura del ambiente");
+        temperatura=scan.nextDouble();
+
+        System.out.println("En conclusion: ");
+
+        if (nevando|| lloviendo|| temperatura<10){
+            System.out.println("Nos quedamos en casa!");
+        } else {
+            System.out.println("Vamos afuera!");
+           
+        }
+              
+    }
+}
